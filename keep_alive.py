da7845d18cb174b9d5f303074f80fea0f974759e
AnimeVerse972 <soibjonovdiyorbek0@gmail.com>
2025-06-17 18:12:10 +0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "Bot doimiy ishlamoqda!"
 
 def keep_alive():
     t = Thread(target=lambda: app.run(host='0.0.0.0', port=8080))
